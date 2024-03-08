2313edd84f7f2a7e7030d0427077540818df9afe
Biswajit Pasi <pasib888@gmail.com>
2024-03-08 13:33:10 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            //string[] ports = SerialPort.GetPortNames();
+
+            //foreach (string element in ports)
+            //{
+            //    Console.WriteLine(element + "\r\n");
+            //}
+
+            SerialPortManager manager = new SerialPortManager("COM1", "COM2");
+            manager.ConfigurePorts();
+            await manager.ReadWriteDataInParallel();
+        }
+        //private static void Main(string[] args)
+        //{
+        //    // Get a list of serial port names.
+        //    using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+        //    {
+        //        var portnames = SerialPort.GetPortNames();
+        //        var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+        //        var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+        //        foreach (string s in portList)
+        //        {
+        //            Console.WriteLine(s);
+        //        }
+        //    }
+        //}
+
+    }
+}
